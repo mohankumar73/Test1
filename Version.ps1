5125b95a0a1959698f657f73ce56e183075c470d
mohansgit <mohankumar55573@gmail.com>
2021-08-18 13:45:26 +0530

    added script file

---
+++ Version.ps1
@@ -0,0 +1,14 @@
+﻿$FilePath = 'C:\Users\mohan\Downloads\Test.targets'
+[xml]$XmlDocument = Get-Content $FilePath
+
+$versionPrint = $XmlDocument.Project.Target.CopyPluginUtilDependencies
+
+Write-Output $versionPrint
+
+
+$DMPkgVer = $versionPrint.DomainMetaPackageVersion
+
+$PluginVer = $versionPrint.PluginUtilVersion
+
+
+#Write-Host "##vso[task.setvariable DomainMetaPackageVersion=$DMPkgVer]secondValue"
